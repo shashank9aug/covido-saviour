766bbdeaaed4cb26cdbbc7909e68daf6b8cb14e8
shashank <shekhar1245shashank@gmail.com>
2021-05-29 20:01:31 +0530

    render homepage

--- app.js
+++ app.js
@@ -12,13 +12,18 @@ app.use(express.static("public"));
 const mongoose = require("mongoose")
 
 
-
 const port=3000
-
 app.get("/",(req,res)=>{
     res.render("home")
 })
 
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+
+app.get("/contact",(req,res)=>{
+    res.render("contact")
+})
 
 app.listen(port,(req,res)=>{
     console.log("server started localhost:3000");
